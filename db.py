81b582db8f788f7adf8cd8308d0abe070333989a
Leyton D'silva <36739259+leytondsilva@users.noreply.github.com>
2020-09-16 19:33:04 +0530

    Python Mysql Connect

---
+++ db.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="videobrowsing"
+  )
+  mycursor = mydb.cursor()
+
+  query = ("SELECT * FROM request")
+
+  mycursor.execute(query)
+
+  myresult = mycursor.fetchall()
+
+  for x in myresult:
+    print(x)
+
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  mycursor.close()
+  mydb.close()
+
+
